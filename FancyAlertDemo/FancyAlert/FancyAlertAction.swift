07baee44cf5ec914d770687c20e3ce6843067164
chaselan <865770853@qq.com>
2018-03-22 16:09:04 +0800

    public init

--- FancyAlertDemo/FancyAlert/FancyAlertAction.swift
+++ FancyAlertDemo/FancyAlert/FancyAlertAction.swift
@@ -20,5 +20,11 @@ public struct FancyAlertAction {
     public var title: String
     public var style: FancyAlertActionStyle
     public var action: (() -> Void)?
+
+    public init(title: String, style: FancyAlertActionStyle, action: (() -> Void)?) {
+        self.title = title
+        self.style = style
+        self.action = action
+    }
     
 }
